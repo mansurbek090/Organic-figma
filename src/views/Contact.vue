6e855d072d04d9bfb4c34a3aab2ebfa65a0ffab9
mansurbek090 <karimov.karim.ru@gmail.com>
2025-05-29 13:50:14 +0500

    organic tugadi

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -45,7 +45,7 @@
           content of a page when looking a layout. The point of using.
         </p>
         <div>
-          <img src="../assets/contactImages/Icon.svg" alt="icon" 
+          <img src="../assets/contactImages/Icon.svg" alt="icon" />
           <p>
             <span>New York, USA:</span> 299 Park Avenue New York, New York 10171
           </p>
@@ -70,8 +70,7 @@
         Your Email* <br />
         <input type="email" placeholder="example@yourmail.com" />
       </label>
-      <label
-        >Company* <br />
+      <label>Company* <br />
         <input type="text" placeholder="yourcompany name here" />
       </label>
       <label>
@@ -79,11 +78,7 @@
       </label>
       <label>
         Message* <br />
-        <textarea
-          placeholder="hello there,i would like to talk about how to..."
-          cols="115%"
-          rows="15px"
-        ></textarea>
+        <textarea placeholder="hello there,i would like to talk about how to..." cols="115%" rows="15px"></textarea>
       </label>
       <button>Send Message</button>
     </form>
@@ -105,13 +100,15 @@ export default {}
   background-repeat: no-repeat;
   background-size: cover;
 }
-.contact-hero > h1 {
+
+.contact-hero>h1 {
   font-family: Roboto, sans-serif;
   font-weight: 800;
   font-size: calc(60 / 19.2 * 1vw);
   line-height: 100%;
   color: #274c5b;
 }
+
 /*     contact-one      */
 .contact-one {
   padding: calc(209 / 19.2 * 1vw) calc(260 / 19.2 * 1vw) calc(98 / 19.2 * 1vw);
@@ -119,31 +116,36 @@ export default {}
   align-items: center;
   column-gap: calc(60 / 19.2 * 1vw);
 }
-.contact-one > img {
+
+.contact-one>img {
   width: calc(624 / 19.2 * 1vw);
   height: calc(680 / 19.2 * 1vw);
   border-radius: calc(20 / 19.2 * 1vw);
 }
-.contact-one-text > h2 {
+
+.contact-one-text>h2 {
   font-family: Roboto, sans-serif;
   font-weight: 800;
   font-size: calc(45 / 19.2 * 1vw);
   line-height: 100%;
   color: #274c5b;
 }
-.contact-one-text > p {
+
+.contact-one-text>p {
   padding: calc(14 / 19.2 * 1vw) 0 calc(34 / 19.2 * 1vw);
   font-family: Open Sans, sans-serif;
   font-weight: 400;
   font-size: calc(18 / 19.2 * 1vw);
   line-height: 165%;
 }
+
 .cards {
   display: flex;
   flex-direction: column;
   row-gap: calc(17 / 19.2 * 1vw);
 }
-.cards > div {
+
+.cards>div {
   width: calc(352 / 19.2 * 1vw);
   padding: calc(11 / 19.2 * 1vw) calc(12 / 19.2 * 1vw);
   display: flex;
@@ -152,21 +154,24 @@ export default {}
   border: calc(1 / 19.2 * 1vw) solid #eeeeee;
   border-radius: calc(11 / 19.2 * 1vw);
 }
-.cards > div > img {
+
+.cards>div>img {
   padding: calc(26 / 19.2 * 1vw) calc(22 / 19.2 * 1vw);
   width: calc(89 / 19.2 * 1vw);
   height: calc(89 / 19.2 * 1vw);
   border-radius: calc(11 / 19.2 * 1vw);
   background: #f4f4f4;
 }
-.cards > div > div > h6 {
+
+.cards>div>div>h6 {
   font-family: Roboto, sans-serif;
   font-weight: 800;
   font-size: calc(25 / 19.2 * 1vw);
   line-height: 100%;
   color: #274c5b;
 }
-.cards > div > div > p {
+
+.cards>div>div>p {
   font-family: Open Sans, sans-serif;
   font-weight: 400;
   font-size: calc(18 / 19.2 * 1vw);
@@ -180,6 +185,7 @@ export default {}
   align-self: center;
   column-gap: calc(15 / 19.2 * 1vw);
 }
+
 .links {
   width: calc(100 / 19.2 * 1vw);
   height: calc(100 / 19.2 * 1vw);
@@ -189,7 +195,8 @@ export default {}
 .conatct-two {
   padding: 0 calc(260 / 19.2 * 1vw) calc(80 / 19.2 * 1vw);
 }
-.conatct-two > div {
+
+.conatct-two>div {
   padding: calc(122 / 19.2 * 1vw);
   border-radius: calc(20 / 19.2 * 1vw);
   width: 100%;
@@ -202,14 +209,16 @@ export default {}
   flex-direction: column;
   align-items: end;
 }
-.conatct-two > div > div {
+
+.conatct-two>div>div {
   border-radius: calc(30 / 19.2 * 1vw);
   background: #ffffff;
   padding: calc(46 / 19.2 * 1vw) calc(53 / 19.2 * 1vw);
   width: calc(514 / 19.2 * 1vw);
   height: 514;
 }
-.conatct-two > div > div > h6 {
+
+.conatct-two>div>div>h6 {
   font-family: Yellowtail, sans-serif;
   font-weight: 400;
   font-size: calc(36 / 19.2 * 1vw);
@@ -217,7 +226,8 @@ export default {}
   letter-spacing: 0%;
   color: #7eb693;
 }
-.conatct-two > div > div > h3 {
+
+.conatct-two>div>div>h3 {
   padding: calc(5 / 19.2 * 1vw) 0 calc(10 / 19.2 * 1vw);
   font-family: Roboto, sans-serif;
   font-weight: 800;
@@ -225,26 +235,31 @@ export default {}
   line-height: 100%;
   color: #274c5b;
 }
-.conatct-two > div > div > p {
+
+.conatct-two>div>div>p {
   font-family: Open Sans, sans-serif;
   font-weight: 400;
   font-size: calc(18 / 19.2 * 1vw);
   line-height: 165%;
   color: #525c60;
 }
-.conatct-two > div > div > div {
+
+.conatct-two>div>div>div {
   padding-top: calc(20 / 19.2 * 1vw);
   display: flex;
   align-items: start;
   column-gap: calc(6 / 19.2 * 1vw);
 }
-.conatct-two > div > div > div > img {
+
+.conatct-two>div>div>div>img {
   width: calc(50 / 19.2 * 1vw);
 }
-.conatct-two > div > div > div > p > span {
+
+.conatct-two>div>div>div>p>span {
   color: #274c5b;
 }
-.conatct-two > div > div > div > p {
+
+.conatct-two>div>div>div>p {
   width: calc(246 / 19.2 * 1vw);
   font-family: Open Sans;
   font-weight: 700;
@@ -252,24 +267,28 @@ export default {}
   line-height: 165%;
   color: #525c60;
 }
+
 /*       eamil-form          */
 .email-form {
   padding: 0 calc(368 / 19.2 * 1vw);
 }
-.email-form > form {
+
+.email-form>form {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
   gap: calc(46 / 19.2 * 1vw) calc(50 / 19.2 * 1vw);
 }
-.email-form > form > label {
+
+.email-form>form>label {
   font-family: Roboto, sans-serif;
   font-weight: 600;
   font-size: calc(18 / 19.2 * 1vw);
   line-height: 100%;
   color: #274c5b;
 }
-.email-form > form > label > input {
+
+.email-form>form>label>input {
   margin-top: calc(15 / 19.2 * 1vw);
   width: calc(539 / 19.2 * 1vw);
   height: auto;
@@ -278,7 +297,8 @@ export default {}
   padding: calc(28 / 19.2 * 1vw) calc(23 / 19.2 * 1vw);
   outline: none;
 }
-.email-form > form > label > input:placeholder {
+
+.email-form>form>label>input:placeholder {
   font-family: Roboto, sans-serif;
   font-weight: 400;
   font-style: italic;
@@ -286,7 +306,8 @@ export default {}
   line-height: 100%;
   color: #ababab;
 }
-.email-form > form > label > textarea {
+
+.email-form>form>label>textarea {
   width: 100%;
   margin-top: calc(25 / 19.2 * 1vw);
   border-radius: calc(16 / 19.2 * 1vw);
@@ -294,7 +315,8 @@ export default {}
   outline: none;
   padding: calc(28 / 19.2 * 1vw) calc(23 / 19.2 * 1vw) 0;
 }
-.email-form > form > label > textarea:placeholder {
+
+.email-form>form>label>textarea:placeholder {
   font-family: Roboto, sans-serif;
   font-weight: 400;
   font-style: italic;
@@ -302,7 +324,8 @@ export default {}
   line-height: 100%;
   color: #ababab;
 }
-.email-form > form > button {
+
+.email-form>form>button {
   padding: calc(28 / 19.2 * 1vw) calc(43 / 19.2 * 1vw);
   border-radius: calc(16 / 19.2 * 1vw);
   background: #274c5b;
